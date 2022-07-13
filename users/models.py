f09b4341697fed381bcda26bc36427a5d8d9f0f2
Reza jeff <niceman.82@yahoo.com>
2022-07-13 17:17:55 +0430

    [feat] user model extended

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    national_code = models.PositiveIntegerField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+
