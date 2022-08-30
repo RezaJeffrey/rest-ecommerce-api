745ef9785f85b5a5f337ab34c4a1718e47c292ed
Cyzer <icegamer.82@yahoo.com>
2022-08-30 17:20:46 +0430

    [feat] add category app and model

---
+++ category/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+app_name = 'category'
+urlpatterns = []
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.api.v1.urls', namespace='api:v1')),
+    path('category', include('category.urls', namespace='category')),
 ]
 
