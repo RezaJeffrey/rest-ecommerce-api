07cd332902705ab7a276efaf844311dfd6f32476
Cyzer <icegamer.82@yahoo.com>
2022-09-02 02:18:23 +0430

    add category api(not completed yet)

---
+++ category/api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import AllCategoriesListView
+
+
+app_name = 'categories:api:v1'
+urlpatterns = [
+    path('categories/', AllCategoriesListView.as_view(), name='categories'),
+]
---
+++ category/api/v1/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from category.models import Category
+
+
+
+
