ae17a9392458f6034e7e46e3b9b52a57214085d8
Cyzer <icegamer.82@yahoo.com>
2022-09-04 01:06:19 +0430

    n

--- category/api/v1/serializers.py
+++ category/api/v1/serializers.py
@@ -2,7 +2,7 @@ from category.models import Category
 from rest_framework import serializers
 
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['name', 'image', 'parent']
