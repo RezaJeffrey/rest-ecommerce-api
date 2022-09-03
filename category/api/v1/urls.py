81870445889c9587dae1ba65e7da3eb27a27f057
unknown <amirrezaahadzzaadeh@yahoo.com>
2022-09-03 23:53:07 +0430

    fix conflicts

--- category/api/v1/urls.py
+++ category/api/v1/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import(AllCategoriesListView, CategoryCreateView
-                   )
+
+# from .views import AllCategoriesListView
 
 app_name = 'categories:api:v1'
 urlpatterns = [
-    path('categories/', AllCategoriesListView.as_view(), name='categories'),
+    # path('categories/', AllCategoriesListView.as_view(), name='categories'),
 ]
--- category/models.py
+++ category/models.py
@@ -10,7 +10,8 @@ class Category(MPTTModel):
     class MPTTMeta:
         order_insertion_by = ('name', )
 
-
+    def __str__(self):
+        return self.name
 
 
 
