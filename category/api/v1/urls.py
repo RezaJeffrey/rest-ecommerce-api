6eacbe239eb18f276e587f9f2b9f1c87ea6c8a6d
unknown <amirrezaahadzzaadeh@yahoo.com>
2022-09-04 00:53:12 +0430

    add category create api!

--- category/api/v1/urls.py
+++ category/api/v1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework import routers
+from .views import AllCategoriesListView, CategoryCreateView
 
-# from .views import AllCategoriesListView
-
+router = routers.DefaultRouter()
 app_name = 'v1'
+
 urlpatterns = [
-    # path('categories/', AllCategoriesListView.as_view(), name='categories'),
+    path('categories/', AllCategoriesListView.as_view(), name='categories'),
+    path('category_create/', CategoryCreateView.as_view(), name='category_create')
 ]
+
