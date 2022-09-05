2d2e850ada4db6b1e2c975e082aafa29fa453fc8
Cyzer <icegamer.82@yahoo.com>
2022-09-06 00:21:13 +0430

    [test] add category model content tests

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from category.models import Category
 
-# Create your tests here.
+
+class TestCategory(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.parent = Category.objects.create(
+            name="parentTestName",
+            parent=None,
+        )
+        cls.category = Category.objects.create(
+            name="categoryNameTest",
+            parent=cls.parent  # pycharm unresolved error(test is runing well I have no Idea:))
+        )  # TODO ask mehran for static files test
+
+    def test_category_model_name_success(self):
+        self.assertEqual(self.category.name, "categoryNameTest")
+
+    def test_category_model_name_failure(self):
+        self.assertNotEqual(self.category.name, "wrong_name")
+
+    def test_category_model_string(self):
+        self.assertEqual(str(self.category), "categoryNameTest")
+
+    def test_category_model_parent_name(self):
+        self.assertEqual(self.category.parent.name, "parentTestName")
+
+    def test_category_model_parents_parent(self):
+        self.assertEqual(self.category.parent.parent, None)
