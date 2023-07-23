5b270940665613fa5fcefc3fb7e86dbf0aaab3f7
amilion <amirrezaahadzadeh1@gmail.com>
2023-07-23 17:55:12 +0330

    fix some bugs in extra fields app!

--- discount/api/v1/views.py
+++ discount/api/v1/views.py
@@ -1,11 +1,24 @@
 from .serializers import DiscountCodeSerializer, ProductDiscountSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView
 from discount.models import DiscountCode
 from core.permissions import IsSeller
+from productpacks.models import ProductPack
+from django.shortcuts import get_object_or_404
 
 
 class DiscountCodeViewSet(ModelViewSet):
     queryset = DiscountCode.objects.all()
     permission_classes = [IsSeller]
     serializer_class = DiscountCodeSerializer
+
+
+class ProductDiscountView(ListCreateAPIView):
+    permission_classes = [IsSeller]
+    serializer_class = ProductDiscountSerializer
+
+    def list(self, request, product_pack_pk):
+        product_pack = get_object_or_404(ProductPack, pk = product_pack_pk)
+        user = request.user
+        
 
