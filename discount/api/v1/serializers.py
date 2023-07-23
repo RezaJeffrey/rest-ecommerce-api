a39f30bd96c7ff58a2e9ece2b74d4fa8c38b6aed
amilion <amirrezaahadzadeh1@gmail.com>
2023-07-23 14:46:42 +0330

    write discounts app serializers, fix some migrations issues, fix model structure of discounts app!

--- discount/api/v1/serializers.py
+++ discount/api/v1/serializers.py
@@ -1,1 +1,43 @@
+from discount.models import DiscountCode, ProductDiscount
+from rest_framework import  serializers
 
+
+class DiscountCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DiscountCode
+        fields = [
+            "code", "available_untill"
+        ]
+
+    def create(self, validated_data):
+        discount_code = DiscountCode.objects.create(**validated_data)
+        return discount_code
+
+    def update(self, instance, validated_data):
+        instance.code = validated_data.get("code", instance.code)
+        instance.available_untill = validated_data.get("available_untill", instance.available_untill)
+        instance.save()
+        return instance
+
+
+class ProductDiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductDiscount
+        fields = [
+            "percent"
+        ]
+
+    def create(self, validated_data, product_pack, discount_code):
+        new_price = product_pack.price * validated_data["percent"] / 100
+        product_discount = ProductDiscount.objects.create(
+            product_pack,
+            discount_code,
+            new_price,
+            validated_data["percent"]
+        )
+        return product_discount
+
+    def update(self, instance, validated_data):
+        instance.percent = validated_data.get("percent", instance.percent)
+        instance.save()
+        return instance
--- discount/models.py
+++ discount/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from datetimemixin.models import DateTimeMixin
-from products.models import Product
+from productpacks.models import ProductPack
 from blacklists.models import Blacklist
+
 
 # discount on single products using code/ discount by price
 class DiscountCode(DateTimeMixin):
@@ -10,9 +11,9 @@ class DiscountCode(DateTimeMixin):
 
 
 class ProductDiscount(DateTimeMixin):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_pack = models.ForeignKey(ProductPack, on_delete=models.CASCADE)
     discount_code = models.ForeignKey(DiscountCode, on_delete=models.CASCADE)
-    new_price = models.DecimalField(decimal_places=2)
+    new_price = models.DecimalField(decimal_places=2, max_digits=14)
     percent = models.PositiveIntegerField(default=0)
     black_list = models.ForeignKey(
         to=Blacklist,
