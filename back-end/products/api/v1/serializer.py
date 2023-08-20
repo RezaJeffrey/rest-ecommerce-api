0f3a3968e43bf55f012309173373b852abedb875
amilion <amirrezaahadzadeh1@gmail.com>
2023-08-20 13:59:46 +0330

    create add image to products endpoint and serve them for frontside in back-end!

--- back-end/products/api/v1/serializer.py
+++ back-end/products/api/v1/serializer.py
@@ -1,11 +1,30 @@
-from products.models import Product
+from products.models import Product, ProductImage
 from extra_fields.models import ExtraFieldValue, ExtraFieldName
 from category.models import Category
 from rest_framework import serializers
 from likes.api.v1.serializer import LikeSerializer
 from comments.api.v1.serializer import CommentSerializer
 from extra_fields.api.v1.serializer import ExtraFieldSerializer
+from django.shortcuts import get_object_or_404
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = [
+            "image", "alt_text"
+        ]
+
+class ProductImageCreateSerializer(serializers.ModelSerializer):
+    product_sku = serializers.CharField(max_length = 255)
+    class Meta:
+        model = ProductImage
+        fields = [
+            "product_sku", "image",
+            "alt_text" 
+        ]
+    def create(self, validated_data):
+        product = get_object_or_404(Product, sku=validated_data.get("product_sku"))
+        return ProductImage.objects.create(product=product, image=validated_data.get("image"), alt_text=validated_data.get("alt_text"))
 
 class ProductSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(
@@ -13,13 +32,14 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     likes = LikeSerializer(many=True)
     comments = CommentSerializer(many=True)
+    images = ProductImageSerializer(many=True)
 
     class Meta:
         model = Product
         fields = [
             'name', 'category',
             'description', 'brand',
-            'shop',
+            'shop', 'images',
             'likes', 'comments'
         ]
 
@@ -30,7 +50,7 @@ class ProductCreateSerializer(serializers.ModelSerializer):
         fields = [
             'name', 'category',
             'description', 'brand',
-            'shop',
+            'shop'
         ]
 
     def create(self, validated_data):
